7f4b2437b24b2454f8c63122934d385f23e5d086
Lucan Chen <chenlucan@hotmail.com>
2015-06-10 15:55:46 +0800

    Formatter handles received data and print out the correct format

--- src/listen/main.cc
+++ src/listen/main.cc
@@ -8,6 +8,33 @@
 
 #include "config/config.h"
 
+class Formatter {
+public:
+  static void to_json(std::string data) {
+    std::cout << data << std::endl;
+  };
+
+  static void to_table(std::string data) {
+    static int title = 0;
+    if (title == 0) {
+      title++;
+      std::cout << "|ticker|bid|ask|bidqty|askqty|last|turnover|" << std::endl;
+    }
+
+    jsonxx::Object o;
+    auto ret = o.parse(data);
+
+    auto ticker   = o.get<jsonxx::String>("ticker", "");
+    auto bidqty   = o.get<jsonxx::Number>("bid_qty", 0);
+    auto askqty   = o.get<jsonxx::Number>("ask_qty", 0);
+    auto bid      = std::atof(o.get<jsonxx::String>("bid_price", "0").c_str());
+    auto ask      = std::atof(o.get<jsonxx::String>("ask_price", "0").c_str());
+    auto last     = std::atof(o.get<jsonxx::String>("last", "0").c_str());
+    auto turnover = std::atof(o.get<jsonxx::String>("turnover", "0").c_str());
+    std::cout << "\r" << ticker << "|" << bid << "|" << ask << "|" << bidqty << "|" << askqty << "|" << last << "|" << turnover << "|" << std::flush;
+  };
+};
+
 int main(int argc, char* argv[]) {
   config::Config config;
   std::string operation = "", option = "";
@@ -46,11 +73,10 @@ int main(int argc, char* argv[]) {
   boost::asio::io_service io_service;
   common::Client client(io_service, "127.0.0.1", port, [=] (std::string data) {
     if (format == 1) {
-      // json format
+      Formatter::to_json(data);
     } else if (format == 2) {
-      // table format
-    }
-  });
+      Formatter::to_table(data);
+    }});
   client.connect();
 
   io_service.run();
